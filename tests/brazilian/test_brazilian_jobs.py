8653e7b5944dde14ceb8b86fe94300eaf774b6b6
Aléctor Alexander <alector.alexandre@gmail.com>
2022-11-07 17:42:09 -0300

    req 11

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,21 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    path = "tests/mocks/brazilians_jobs.csv"
+    response = read_brazilian_file(path)
+    assert response[0] == {
+        "title": "Maquinista",
+        "salary": "2000",
+        "type": "trainee",
+    }
+    assert response[1] == {
+        "title": "Motorista",
+        "salary": "3000",
+        "type": "full time",
+    }
+    assert response[2] == {
+        "title": "Analista de Software",
+        "salary": "4000",
+        "type": "full time",
+    }
