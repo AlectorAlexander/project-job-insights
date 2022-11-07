a7c889db8a177d7618c063a326e25c861c334574
Aléctor Alexander <alector.alexandre@gmail.com>
2022-11-07 18:05:38 -0300

    req02

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,30 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+
+jobs = [
+    {
+        "min_salary": 10000,
+        "max_salary": 24000,
+        "date_posted": "2022-01-12",
+    },
+    {
+        "min_salary": 28000,
+        "max_salary": 42000,
+        "date_posted": "2024-07-31",
+    },
+    {
+        "min_salary": 15000,
+        "max_salary": 23000,
+        "date_posted": "2021-06-06",
+    },
+]
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(jobs, "min_salary")
+    assert jobs[0]["min_salary"] == 10000
+
+    sort_by(jobs, "max_salary")
+    assert jobs[0]["max_salary"] == 42000
+
+    sort_by(jobs, "date_posted")
+    assert jobs[0]["date_posted"] == "2024-07-31"
