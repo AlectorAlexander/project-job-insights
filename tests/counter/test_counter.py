08b0579ebd871fe9e8e7a6dd38db8cbf75b5a94a
Aléctor Alexander <alector.alexandre@gmail.com>
2022-11-07 16:50:11 -0300

    req10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = "src/jobs.csv"
+    word = "Python"
+    counter = count_ocurrences(path, word)
+    assert counter == 1570
